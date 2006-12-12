d6ae2a1c43393748f17077f1abdf240de44370eb
mik <mik@e876e4f3-65f1-0310-8132-c7840cfc2588>
2006-12-12 11:53:20 +0000

    * [goil] update for galgas 1.5.6   * [goil] update counters for C166 target.   * update default Keil project (uv2 file)

--- trunk/App/tpl_default_tasks_c167.c
+++ trunk/App/tpl_default_tasks_c167.c
@@ -1,53 +1,15 @@
 #include "../os/tpl_os.h"
 #include "tpl_os_generated_configuration.h"
+#include "tpl_os_wrappers.h"
 #include <stdio.h>
 
 #include <C167CS.H>
 
-sbit LED=P8^0;
-sbit LED1=P8^1;
+sbit LED=P7^0;
+sbit LED1=P7^1;
 
 void InitApp(void)
 {
-}
-
-void damned(void)
-{
-}
-
-void ErrorHook(StatusType error)
-{
-}
-
-void PreTaskHook(void)
-{
-    TaskType id;
-    GetTaskID(&id);
-    printf("Avant %d\n",id);
-}
-
-void PostTaskHook(void)
-{
-    TaskType id;
-    GetTaskID(&id);
-    printf("Apres %d\n",id);
-}
-
-void StartupHook(void)
-{
-    printf("Ca demarre !\n");
-}
-
-void ShutdownHook(StatusType error)
-{
-    printf("Au revoir et a bientot :)\n");
-}
-
-ISR2(it_trap32)
-{
-	LED1=!LED1;
-	ActivateTask(tick);
-	TerminateISR2();
 }
 
 TASK(tick)
@@ -56,10 +18,10 @@ TASK(tick)
     TerminateTask();
 }
 
+void tpl_init_tick_timer();
 TASK(init)
 {
-	T01CON = 0x0045; /* period 840 ms. Timer 0*/ 
-	T0IC = 0x0050;   /* IE=1, ILVL 4, GLVL 0  */
-    TerminateTask();
+	tpl_init_tick_timer();
+	TerminateTask();
 }
 
