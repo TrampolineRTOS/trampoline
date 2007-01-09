4dc33660d8c52f4f67f98555253a35331cb6343b
ilias <ilias@e876e4f3-65f1-0310-8132-c7840cfc2588>
2007-01-09 10:36:46 +0000

    added documentation for hooks

--- trunk/os/tpl_os_hooks.h
+++ trunk/os/tpl_os_hooks.h
@@ -1,4 +1,8 @@
-/*
+/**
+ * @file tpl_os_hooks.h
+ *
+ * @section copyright Copyright
+ *
  * Trampoline OS
  *
  * Trampoline is copyright (c) IRCCyN 2005+
@@ -6,13 +10,16 @@
  *
  * This software is distributed under the Lesser GNU Public Licence
  *
- * Trampoline Hooks macros, functions and datatypes header
+ * @section infos File informations
  *
  * $Date$
  * $Rev$
  * $Author$
  * $URL$
  *
+ * @section descr File description
+ *
+ * Trampoline Hooks related macros, functions and datatypes
  */
 
 #ifndef __TPL_OS_HOOKS_H__
@@ -27,6 +34,11 @@
 #   endif
 #endif
 
+/**
+ * @def CALL_POST_TASK_HOOK
+ *
+ * Generates a call to #PostTaskHook only if WITH_POST_TASK_HOOK (or WITH_TASK_HOOK) is defined
+ */
 #ifdef WITH_POST_TASK_HOOK
 #   define CALL_POST_TASK_HOOK()    \
     PostTaskHook();
@@ -34,6 +46,11 @@
 #   define CALL_POST_TASK_HOOK()
 #endif
 
+/**
+ * @def CALL_PRE_TASK_HOOK
+ *
+ * Generates a call to #PreTaskHook only if WITH_PRE_TASK_HOOK (or WITH_TASK_HOOK) is defined
+ */
 #ifdef WITH_PRE_TASK_HOOK
 #   define CALL_PRE_TASK_HOOK()    \
     PreTaskHook();
@@ -41,6 +58,11 @@
 #   define CALL_PRE_TASK_HOOK()
 #endif
 
+/**
+ * @def CALL_STARTUP_HOOK
+ *
+ * Generates a call to #StartupHook only if WITH_STARTUP_HOOK is defined 
+ */
 #ifdef WITH_STARTUP_HOOK
 #   define CALL_STARTUP_HOOK()  \
     StartupHook();
@@ -48,6 +70,13 @@
 #   define CALL_STARTUP_HOOK()
 #endif
 
+/**
+ * @def CALL_SHUTDOWN_HOOK
+ *
+ * Generates a call to #ShutdownHook only if WITH_SHUTDOWN_HOOK is defined
+ *
+ * @param error error code (#StatusType) to send to shutdown hook
+ */
 #ifdef WITH_SHUTDOWN_HOOK
 #   define CALL_SHUTDOWN_HOOK(error)  \
     ShutdownHook(error);
