8876eab164dde92ab5d1eaf8988d9f6be227a14f
ilias <ilias@e876e4f3-65f1-0310-8132-c7840cfc2588>
2007-01-09 14:20:25 +0000

    added doxygen documentation for tpl_os_wrappers.h

--- trunk/os/tpl_os_wrappers.h
+++ trunk/os/tpl_os_wrappers.h
@@ -1,4 +1,8 @@
-/*
+/**
+ * @file tpl_os_wrappers.h
+ *
+ * @section copyright Copyright
+ *
  * Trampoline OS
  *
  * Trampoline is copyright (c) IRCCyN 2005+
@@ -6,22 +10,55 @@
  *
  * This software is distributed under the Lesser GNU Public Licence
  *
- * Result codes of the OS
+ * @section infos File informations
  *
  * $Date: 2006-12-03 16:27:57 +0100 (dim., 03 déc. 2006) $
  * $Rev: 225 $
  * $Author: mik $
  * $URL: https://trampoline.rts-software.org/svn/trunk/os/tpl_os_definitions.h $
  *
+ * @section descr File description
+ *
+ * Macros in this file are wrappers which gives the ability to call a service
+ * specifying directly a task name (instead of a TaskType value or variable).
  */
 
 #ifndef __TPL_OS_WRAPPERS_H__
 #define __TPL_OS_WRAPPERS_H__
 
+/**
+ * @def ActivateTask
+ *
+ * @see #ActivateTask
+ */
 #define ActivateTask(taskId) ActivateTask(task_id_of_##taskId##)
+
+/**
+ * @def ChainTask
+ *
+ * @see #ChainTask
+ */
 #define ChainTask(taskId) ChainTask(task_id_of_##taskId##)
+
+/**
+ * @def GetTaskState
+ *
+ * @see #GetTaskState
+ */
 #define GetTaskState(taskId,state) GetTaskState(task_id_of_##taskId##,state)
+
+/**
+ * @def setEvent
+ *
+ * @see #SetEvent
+ */
 #define setEvent(taskId,event) setEvent(task_id_of_##taskId##,event)
+
+/**
+ * @def getEvent
+ *
+ * @see #GetEvent
+ */
 #define getEvent(taskId,event) getEvent(task_id_of_##taskId##,event)
 
 #endif
