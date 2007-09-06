88294283b4c0ffb3159ef47a7876225756d89cb5
ilias <ilias@e876e4f3-65f1-0310-8132-c7840cfc2588>
2007-09-06 14:31:52 +0000

    added missing protection hook files

---
+++ autosar/tpl_as_protec_hook.c
@@ -0,0 +1,69 @@
+/**
+ * @file tpl_as_protec_hook.c
+ *
+ * @section desc File description
+ *
+ * TODO
+ *
+ * @section copyright Copyright
+ *
+ * Trampoline OS
+ *
+ * Trampoline is copyright (c) IRCCyN 2005-2007
+ * Autosar extension is copyright (c) IRCCyN and ESEO 2007
+ * Trampoline and its Autosar extension are protected by the
+ * French intellectual property law.
+ *
+ * This software is distributed under the Lesser GNU Public Licence
+ *
+ * @section infos File informations
+ *
+ * $Date$
+ * $Rev$
+ * $Author$
+ * $URL$
+ */
+#include "tpl_as_protec_hook.h"
+#include "tpl_dow.h"
+#include "tpl_os_kernel.h"
+
+static void killtaskisr ()
+{
+  tpl_running_obj->state = (tpl_exec_state)DYING;
+  tpl_schedule((u8)FROM_IT_LEVEL);
+}
+ 
+void tpl_call_protection_hook (tpl_status error)
+{
+#ifdef WITH_PROTECTION_HOOK
+  ProtectionReturnType result;
+  
+  result = ProtectionHook (error);
+  
+  switch (result)
+  {
+    case PRO_KILLTASKISR:
+      killtaskisr ();
+      break;
+    case PRO_KILLAPPL:
+    /* FIXME: check this behavior */
+      tpl_running_obj->state = (tpl_exec_state)DYING;
+      tpl_schedule((u8)FROM_IT_LEVEL);
+      break;
+    case PRO_KILLAPPL_RESTART:
+    /* FIXME: check this behavior */
+      tpl_running_obj->state = (tpl_exec_state)RESURRECT;
+      tpl_schedule((u8)FROM_IT_LEVEL);
+      break;
+    case PRO_SHUTDOWN:
+      tpl_shutdown ();
+      break;
+    default:
+      killtaskisr ();
+      break;
+  }
+#else /* WITH_PROTECTION_HOOK */
+  killtaskisr ();
+#endif /* WITH_PROTECTION_HOOK */
+}
+
---
+++ autosar/tpl_as_protec_hook.h
@@ -0,0 +1,76 @@
+/**
+ * @file tpl_as_protec_hook.h
+ *
+ * @section desc File description
+ *
+ * @TODO: finish documentation
+ *
+ * @section copyright Copyright
+ *
+ * Trampoline OS
+ *
+ * Trampoline is copyright (c) IRCCyN 2005-2007
+ * Autosar extension is copyright (c) IRCCyN and ESEO 2007
+ * Trampoline and its Autosar extension are protected by the
+ * French intellectual property law.
+ *
+ * This software is distributed under the Lesser GNU Public Licence
+ *
+ * @section infos File informations
+ *
+ * $Date$
+ * $Rev$
+ * $Author$
+ * $URL$
+ */
+#ifndef TPL_AS_PROTEC_HOOK_H
+#define TPL_AS_PROTEC_HOOK_H
+
+#include "tpl_os_application_def.h"
+#include "tpl_os_internal_types.h"
+#include "tpl_os_custom_types.h"
+
+/**
+ * @def E_OS_PROTECTION_TIME
+ * 
+ * A task exeeded its execution budget or an ISR2 exeeded its
+ * exection time.
+ * 
+ * see §7.10 of autosar OS SWS 2.1
+ */
+#define E_OS_PROTECTION_TIME 32 /* FIXME: how values are allocated ? */
+
+/**
+ * @def E_OS_PROTECTION_LOCKED
+ * 
+ * A Task/Category 2 ISR blocks for too long
+ * 
+ * see §7.10 of autosar OS SWS 2.1 
+ */
+#define E_OS_PROTECTION_LOCKED 33 /* FIXME: how values are allocated ? */
+
+typedef enum 
+{
+  PRO_KILLTASKISR,
+  PRO_KILLAPPL,
+  PRO_KILLAPPL_RESTART,
+  PRO_SHUTDOWN
+} ProtectionReturnType;
+
+#ifdef WITH_PROTECTION_HOOK
+/**
+ * User protection hook callback function.
+ * 
+ * @TODO finish documentation
+ * 
+ * see §8.5.1 of AUTOSAR OS SWS 2.1.0
+ */
+extern ProtectionReturnType ProtectionHook (tpl_status FatalError);
+#endif /* WITH_PROTECTION_HOOK */
+
+/**
+ * @TODO
+ */
+extern void tpl_call_protection_hook (tpl_status error);
+
+#endif /* TPL_AS_PROTEC_HOOK_H */
