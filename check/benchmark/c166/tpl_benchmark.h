44efb19344e40411335b828c22cc5c9fec4d0ada
gforget <gforget@e876e4f3-65f1-0310-8132-c7840cfc2588>
2008-05-27 14:14:53 +0000

    [check] delete monitor files modification to serial program bug fix

--- check/benchmark/c166/tpl_benchmark.h
+++ check/benchmark/c166/tpl_benchmark.h
@@ -9,6 +9,3 @@ extern u16 tpl_benchmark_tick;
 #define TPL_BENCHMARK_DIVISOR 10
 #define TPL_BENCHMARK_UNIT 0xB5	   // 0xB5 = µ
 #endif
-
-void tpl_stop_interruption (void);
-void tpl_start_interruption (void);
