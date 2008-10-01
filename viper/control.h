742b4e442fc116e2e65eadc00eebc672277cc2de
Jean-Luc Béchennec <jlbirccyn@users.noreply.github.com>
2008-10-01 15:03:40 +0000

    missing files in viper

---
+++ viper/control.h
@@ -0,0 +1,16 @@
+/*
+ *  control.h
+ *  viper
+ *
+ *  Created by Jean-Luc BÈchennec on 08/02/08.
+ *  Copyright 2008 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef CONTROL_H
+#define CONTROL_H
+
+void init_motors(void);
+void close_motors(void);
+
+#endif
