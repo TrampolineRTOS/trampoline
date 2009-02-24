be3cd753df6b9b505c1f4c7ad8d8971281c76d76
fp <fp@e876e4f3-65f1-0310-8132-c7840cfc2588>
2009-02-24 09:24:40 +0000

    New check application - hook_s1_non

---
+++ check/functional/hook_s1_non/hook_s1.c
@@ -0,0 +1,29 @@
+#include "config.h" /*Display information in the right way (printf on UNIX...)*/
+#include "tpl_os.h"
+#include "embUnit.h"
+
+TestRef HookTest_seq1_startup_instance(void);
+TestRef HookTest_seq1_shutdown_instance(void);
+
+int main(void)
+{
+	StartOS(OSDEFAULTAPPMODE);
+	return 0;
+}
+
+void StartupHook(void)
+{ 
+	TestRunner_start();
+	TestRunner_runTest(HookTest_seq1_startup_instance());
+}
+
+void ShutdownHook(StatusType error)
+{ 
+	TestRunner_runTest(HookTest_seq1_shutdown_instance());
+	TestRunner_end();
+}
+
+TASK(t1)
+{
+	//
+}
