aac1c6aebd9ce267ef311d60f2d3449592e35431
fp <fp@e876e4f3-65f1-0310-8132-c7840cfc2588>
2009-02-26 11:22:43 +0000

    Chek application - autosar test exemple

---
+++ check/functional/autosar_s1/autosar_s1.c
@@ -0,0 +1,29 @@
+#include "config.h" /*Display information in the right way (printf on UNIX...)*/
+#include "tpl_os.h"
+#include "embUnit.h"
+
+TestRef AutosarTest_seq1_t1_instance(void);
+TestRef AutosarTest_seq1_t2_instance(void);
+
+int main(void)
+{
+	StartOS(OSDEFAULTAPPMODE);
+	return 0;
+}
+
+void ShutdownHook(StatusType error)
+{ 
+	TestRunner_end();
+}
+
+TASK(t1)
+{
+	TestRunner_start();
+	TestRunner_runTest(AutosarTest_seq1_t1_instance());
+}
+
+TASK(t2)
+{
+	TestRunner_runTest(AutosarTest_seq1_t2_instance());
+	ShutdownOS(E_OK);
+}
