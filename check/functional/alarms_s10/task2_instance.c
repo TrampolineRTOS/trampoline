02d7650bdb0061cecc7c725f8ec76750e319ee29
fp <fp@e876e4f3-65f1-0310-8132-c7840cfc2588>
2009-02-27 17:09:40 +0000

    Check Application - See ticket #245

---
+++ check/functional/alarms_s10/task2_instance.c
@@ -0,0 +1,27 @@
+/*Instance of task t2*/
+
+#include "embUnit.h"
+#include "tpl_os.h"
+
+
+/*test case:test the reaction of the system called with 
+an activation of a task*/
+static void test_t2_instance(void)
+{
+	int result_inst_1;
+	
+	result_inst_1 = TerminateTask();
+	TEST_ASSERT_EQUAL_INT(E_OK, result_inst_1);
+	
+}
+
+/*create the test suite with all the test cases*/
+TestRef AlarmsTest_seq10_t2_instance(void)
+{
+	EMB_UNIT_TESTFIXTURES(fixtures) {
+		new_TestFixture("test_t2_instance",test_t2_instance)
+	};
+	EMB_UNIT_TESTCALLER(AlarmsTest,"AlarmsTest_sequence10",NULL,NULL,fixtures);
+
+	return (TestRef)&AlarmsTest;
+}
