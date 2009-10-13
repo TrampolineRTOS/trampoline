a9fbd8e4d17423efc8db61c892d0f15d4c3a5caa
fp <fp@e876e4f3-65f1-0310-8132-c7840cfc2588>
2009-10-13 16:12:20 +0000

    Viper2 modifications :   * Create an application (App-GBF)

---
+++ trunk/App-GBF/tpl_default_tasks_workstation.h
@@ -0,0 +1,21 @@
+
+void InitApp(void);
+
+void dac(int value_to_convert);
+
+void start_dialoguing_dac(void);
+
+void stop_dialoguing_dac(void);
+
+void start_dialoguing_lcd(void);
+
+void stop_dialoguing_lcd(void);
+
+void lcd1(char *string);
+
+void lcd2(char *string);
+
+void afficheHeure(const unsigned char minutes, const unsigned char secondes);
+
+void affichePeriode(void);
+
