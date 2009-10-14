e39f53f8360548759f780dd97c3381d9d25aa1a7
fp <fp@e876e4f3-65f1-0310-8132-c7840cfc2588>
2009-10-14 14:25:18 +0000

    Viper2 check on his own python version used and modify library and lib path in Makefile

--- trunk/viper2/viper2.py
+++ trunk/viper2/viper2.py
@@ -1,16 +1,5 @@
-#!/usr/local/bin/python
-
 ###############################################################################
-# check that viper2 ipc lib is compiled.
-###############################################################################
-import os
-try:
-  import ipc
-except:
-  os.system("make all")
-    
-###############################################################################
-# IMPORT
+# Add directories to the list of strings that specifies the search path for modules
 ###############################################################################
 import sys
 sys.path.append('module')
@@ -18,6 +7,43 @@ sys.path.append('device')
 sys.path.append('ipc')
 sys.path.append('widget')
 
+##############################################################################
+# Check that viper2 ipc lib is compiled. Otherwise, compile it and set python paths 
+###############################################################################
+import os
+import commands
+try:
+  import ipc
+except:
+  """ Set python paths (lib and include)
+      in Makefile according to python used  
+  """
+  vers = sys.version_info
+  vers_1 = commands.getoutput("echo '" + str(vers) + "' | awk -F',' '{print $1}' | awk -F'(' '{print $2}'")
+  vers_2 = commands.getoutput("echo '" + str(vers) + "' | awk '{print $2}' | awk -F',' '{print $1}'")
+  #vers_python = "python" + vers_1 + "." + vers_2
+  vers_python = commands.getoutput("echo python" + vers_1 + "." + vers_2)
+
+  #get python (bin) path
+  wich_python = commands.getoutput("which " + vers_python)
+  path_python_bin = commands.getoutput("echo " + wich_python + " | sed 's_n/" + vers_python + "_n_g'")
+
+  #get python include path 
+  path_python_include = commands.getoutput("echo " + path_python_bin + " | sed 's/bin/include/g'")
+  #get python lib path
+  path_python_lib = commands.getoutput("echo " + path_python_bin + " | sed 's/bin/lib/g'")
+
+  #change path in Makefile
+  commands.getoutput("sed 's:PATH_PYTHON_INCLUDE:" + path_python_include + ":g' Makefile > TMPFILE && mv TMPFILE Makefile")
+  commands.getoutput("sed 's:PATH_PYTHON_LIB:" + path_python_lib + ":g' Makefile > TMPFILE && mv TMPFILE Makefile")
+  commands.getoutput("sed 's:VERS_PYTHON:" + vers_python + ":g' Makefile > TMPFILE && mv TMPFILE Makefile")
+
+  """ Make libraries """
+  os.system("make all")
+  
+###############################################################################
+# IMPORT
+###############################################################################
 import signal
 import traceback
 import subprocess
@@ -42,15 +68,39 @@ if "-h" in sys.argv or "--help" in sys.argv:
   print ""
   print "\t-g : Generate vp_ipc_devices.h and target.cfg"
   print "\t-c : Use with -g, compile trampolines"
-  print "\t-m : Use with -m, clean dependencies"
+  print "\t-m : Clean dependencies (before a commit)"
   print "\t--nodep : Use with -c, compile with NODEP=1 option"
 
 #Clean
 elif "-m" in sys.argv or "--mrproper" in sys.argv:
+  """ Clean dependencies """
   os.system("make mrproper")
   for ecu in config.allEcus:
     os.system("cd " + ecu._dir + "; rm -rf build; rm -rf defaultAppWorkstation; rm -rf Make-rules; rm -rf Makefile; rm -rf trampoline; rm -rf target.cfg; rm -rf vp_ipc_devices.h")
+  
+  """ ReSet python paths (lib and include)
+      in Makefile according to python used
+  """
+  vers = sys.version_info
+  vers_1 = commands.getoutput("echo '" + str(vers) + "' | awk -F',' '{print $1}' | awk -F'(' '{print $2}'")
+  vers_2 = commands.getoutput("echo '" + str(vers) + "' | awk '{print $2}' | awk -F',' '{print $1}'")
+  #vers_python = "python" + vers_1 + "." + vers_2
+  vers_python = commands.getoutput("echo python" + vers_1 + "." + vers_2)
 
+  #get python (bin) path
+  wich_python = commands.getoutput("which " + vers_python)
+  path_python_bin = commands.getoutput("echo " + wich_python + " | sed 's_n/" + vers_python + "_n_g'")
+
+  #get python include path 
+  path_python_include = commands.getoutput("echo " + path_python_bin + " | sed 's/bin/include/g'")
+  #get python lib path
+  path_python_lib = commands.getoutput("echo " + path_python_bin + " | sed 's/bin/lib/g'")
+
+  #change path in Makefile
+  commands.getoutput("sed 's:" + path_python_include + ":PATH_PYTHON_INCLUDE:g' Makefile > TMPFILE && mv TMPFILE Makefile")
+  commands.getoutput("sed 's:" + path_python_lib + ":PATH_PYTHON_LIB:g' Makefile > TMPFILE && mv TMPFILE Makefile")
+  commands.getoutput("sed 's:" + vers_python + ":VERS_PYTHON:g' Makefile > TMPFILE && mv TMPFILE Makefile")
+  
 #Generate
 elif "-g" in sys.argv or "--generate" in sys.argv:
   for ecu in config.allEcus:
