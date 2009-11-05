985a56fe0dedeac3c5f84a4a10c26b5ef42d277f
fp <fp@e876e4f3-65f1-0310-8132-c7840cfc2588>
2009-11-05 13:25:34 +0000

    Inserting documentation in trunk/documentation/trampoline_plans/

---
+++ trunk/documentation/trampoline_plans/OIL_to_TXT/OIL_to_txt.sh
@@ -0,0 +1,107 @@
+#!/bin/sh
+
+echo "Generate sequence description files from OIL files..."
+
+if [ "$1" = "-f" ] 
+then
+	force=1
+else
+	force=0
+fi
+
+for first_path in "GOIL" "functional"
+do
+	echo "${first_path} tests sequences"
+	
+	PAT="../../../trampoline-trunk/check/$first_path/"
+
+	for name in `cat ${PAT}${first_path}_testSequences.txt`
+	do
+			
+		copy=0
+		bracket=1
+		full=0
+		end=0
+		date_oil=0
+		date_file=0
+	
+		#if full, do the txt file, otherwise, do nothing
+		if [ "`echo $name | grep -c non`" = "0" ]
+		then
+			if [ "`echo $name | grep -c full`" != "0" ]
+			then
+				full=1
+				file=`echo $name | sed -e "s/_full//g"`
+				#echo "full - modified : $name"
+			else
+				#echo "normal : $name"
+				file=${name}
+			fi	#[ "`echo $name | grep -c full`" != "0" ]
+			
+			#if no modifications since last time, don't create again the file
+			if ( `test -f ${file}.txt` )
+			then
+				#echo "file exists here `pwd`"
+				date_oil=`stat -s ${PAT}${name}/${name}.oil | awk '{print $10}' | awk -F"=" '{print $2}'`
+				date_file=`stat -s ${file}.txt | awk '{print $10}' | awk -F"=" '{print $2}'`
+			#else
+				#echo "file doesn't exist here `pwd`"
+			fi
+			
+			if [ $date_oil -gt $date_file ] || ( ! `test -f ${file}.txt` ) || [ "$force" = "1" ]
+			then		
+				#create a new file
+				> ${file}.txt
+			
+				#copy in the old file the paragraph needed
+				old_IFS=$IFS
+				IFS=$'\n'     # nouveau séparateur de champ, le caractère fin de ligne
+				for ligne in $(cat ${PAT}${name}/${name}.oil)
+				do
+					#if TASK has elapsed, count the { and } and copy until } has one more than {
+					if [ "`echo $copy`" = "2" ]
+					then
+						open_bracket_number=`echo $ligne | grep -c "{"`
+						#echo "open_bracket_number=$open_bracket_number"
+						bracket=`expr $bracket + $open_bracket_number`
+						close_bracket_number=`echo $ligne | grep -c "}"`
+						#echo "close_bracket_number=$close_bracket_number"
+						bracket=`expr $bracket - $close_bracket_number`
+						#echo "bracket=$bracket"
+						if [ "`echo $bracket`" != "-1" ]
+						then
+							echo "$ligne" >> ${file}.txt
+						else
+							if [ "`echo $end`" = "0" ]
+							then
+								end=1
+								echo "${PAT}${name} - added"
+								if [ "`echo $full`" = "1" ]
+								then
+									#if full replace "FULL" by "NON, FULL"
+									#echo "FULL -> NON, FULL"
+									cat ${file}.txt | sed -e "s_FULL_NON,FULL_g" > ${file_backup}.txt
+									mv ${file_backup}.txt ${file}.txt
+								fi
+							fi
+						fi
+					else
+						if [ "`echo $ligne | grep -c 'TASK '`" != "0" ]
+						then
+							copy=`expr $copy + 1`
+							if [ "`echo $copy`" = "2" ]
+							then
+								echo "$ligne" >> ${file}.txt
+							fi #[ "`echo $copy`" = "2" ]
+						fi #[ "`echo $ligne | grep -c 'TASK '`" != "0" ]
+					fi
+				done
+				IFS=$old_IFS
+				
+			fi # [ $date_oil -gt $date_file ] || ( ! `test -f ${file}.txt` )
+			
+		fi # [ "`echo $name | grep -c non`" = "0" ]
+	
+	done
+
+done
---
+++ trunk/documentation/trampoline_plans/generate_pdf.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+plan=0
+proc=0
+impl=0
+oil=0
+
+#check if user wants to compile just the plan or the procedure
+if [ "$1" = "plan" ] 
+then
+	plan=1
+elif [ "$1" = "proc" ] 
+then
+	proc=1
+elif [ "$1" = "impl" ] 
+then
+	impl=1
+elif [ "$1" = "oil" ] 
+then
+	oil=1
+elif [ "$1" = "clean" ] 
+then
+	echo "Cleaning temporary files..."
+	#Trash Aux Files
+	rm Trampolinetest*.aux
+	rm Trampolinetest*.blg
+	rm Trampolinetest*.log
+	rm Trampolinetest*.out
+	rm Trampolinetest*.toc
+	
+	rm Trampolinetest*.bbl
+	rm Trampolinetest*.ent
+	rm Trampolinetest*.gz
+	
+	rm OIL_to_TXT/*.txt
+else
+	plan=1
+	proc=1
+	impl=1
+	oil=1
+fi
+
+#generate sequences description files from OIL files
+if [ $oil = 1 ]
+then
+	cd OIL_to_TXT
+	./OIL_to_TXT.sh
+	cd ..
+fi
+
+#compile what the user want (or all)
+if [ $plan = 1 ]
+then
+	echo "Generating Trampolinetestplan10.pdf..."
+	pdflatex Trampolinetestplan10
+	bibtex Trampolinetestplan10
+	pdflatex Trampolinetestplan10
+	pdflatex Trampolinetestplan10
+fi
+
+if [ $proc = 1 ]
+then
+	echo "Generating Trampolinetestproc10.pdf..."
+	pdflatex Trampolinetestproc10
+	bibtex Trampolinetestproc10
+	pdflatex Trampolinetestproc10
+	pdflatex Trampolinetestproc10
+fi
+
+if [ $impl = 1 ]
+then
+	echo "Generating Trampolinetestimplementation10.pdf..."
+	pdflatex Trampolinetestimplementation10
+	bibtex Trampolinetestimplementation10
+	pdflatex Trampolinetestimplementation10
+	pdflatex Trampolinetestimplementation10
+fi
+
