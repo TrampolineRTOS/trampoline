c714565e5b28b1ac3d8ae760b755244d72bf3462
Jean-Luc Béchennec <jlbirccyn@users.noreply.github.com>
2010-03-11 15:59:59 +0000

    Memory protection is working (at first glance). Extended testing should be done now.

--- examples/ppc/codewarrior-mpc5516/lonely.c
+++ examples/ppc/codewarrior-mpc5516/lonely.c
@@ -37,7 +37,7 @@ VAR(TaskType, OS_VAR) new_task;
 
 #define APP_Task_iamsolonely_START_SEC_CONST_32BIT
 #include "tpl_memmap.h"
-CONST(int, AUTOMATIC) toto = 0;
+CONST(int, AUTOMATIC) tata = 0;
 #define APP_Task_iamsolonely_STOP_SEC_CONST_32BIT
 #include "tpl_memmap.h"
 
@@ -45,6 +45,12 @@ CONST(int, AUTOMATIC) toto = 0;
 #include "tpl_memmap.h"
 VAR(int, AUTOMATIC) titi = 0;
 #define APP_Task_iamsolonely_STOP_SEC_VAR_32BIT
+#include "tpl_memmap.h"
+
+#define APP_Task_gogo_START_SEC_VAR_32BIT
+#include "tpl_memmap.h"
+VAR(int, AUTOMATIC) toto = 0;
+#define APP_Task_gogo_STOP_SEC_VAR_32BIT
 #include "tpl_memmap.h"
 
 #define OS_START_SEC_CODE
@@ -65,9 +71,23 @@ FUNC(void,AUTOMATIC) tpl_trigger_sw_it(void);
 
 TASK(iamsolonely)
 {
-    titi = 1;
+  titi = 1;
+  titi = toto;
+  toto = 4;
 	while(1);
 	TerminateTask();
 }
 #define APP_Task_iamsolonely_STOP_SEC_CODE
+#include "tpl_memmap.h"
+
+#define APP_Task_gogo_START_SEC_CODE
+#include "tpl_memmap.h"
+TASK(gogo)
+{
+  static char led_state = 0;
+  WriteLine(PORT_C,11,led_state);
+  led_state = ! led_state;
+	TerminateTask();
+}
+#define APP_Task_gogo_STOP_SEC_CODE
 #include "tpl_memmap.h"
