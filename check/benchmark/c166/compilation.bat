4931f1c3708530ad302498b245119b0c664bd03b
Jean-Luc Béchennec <jlbirccyn@users.noreply.github.com>
2010-03-16 10:09:48 +0000

    2 check folders, one with viper and one with viper2, step 3

--- check/benchmark/c166/compilation.bat
+++
@@ -1,3 +0,0 @@
-C:\Keil\UV3\UV3  -r part1.UV2
-C:\Keil\UV3\UV3  -r part2.UV2
-pause
--- check/benchmark/ppc/bench_OS/Source/tpl_benchmark.h
+++
@@ -1,9 +0,0 @@
-
-// Benchmark GF 04/08
-#ifdef BENCHMARK
-extern unsigned int tpl_benchmark_tick;
-
-#define TPL_BENCHMARK_MULTIPLIER 4
-#define TPL_BENCHMARK_DIVISOR 10
-#define TPL_BENCHMARK_UNIT 0xB5	   // 0xB5 = µ
-#endif
