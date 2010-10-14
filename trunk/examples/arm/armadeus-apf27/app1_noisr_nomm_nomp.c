5855382b841310605326ebeafc265ba5a95bc571
ilias <ilias@e876e4f3-65f1-0310-8132-c7840cfc2588>
2010-10-14 15:43:44 +0000

    - fixed a bug with exception table - better support of u-boot - lightened APF27 example application (app1)

--- trunk/examples/arm/armadeus-apf27/app1_noisr_nomm_nomp.c
+++ trunk/examples/arm/armadeus-apf27/app1_noisr_nomm_nomp.c
@@ -1,34 +1,17 @@
-#include <stdio.h>
 #include "tpl_os.h"
 #include "tpl_app_config.h"
 
-CONST(u32, AUTOMATIC ) CONST_TO_WRITE = 0x55;
-
-VAR (int, AUTOMATIC) trace = 0;
-VAR(int, AUTOMATIC) var_t1 = 0;
-
-VAR(int, AUTOMATIC) var_t2 = 0;
-P2VAR (u32, AUTOMATIC, OS_CODE) write_code; 
-
-VAR(int, AUTOMATIC) var_t3 = 20;
-P2VAR (u32, AUTOMATIC, OS_CONST) write_const;
-
-VAR(int, AUTOMATIC) var_t4 = 0;
-
-VAR(int, AUTOMATIC) var_it1 = 0;
-
-VAR(int, AUTOMATIC) var_it2 = 0;
-
 FUNC(int, AUTOMATIC) main()
 {
+//		tpl_disable_interrupts ();
 		serial_init();
-		serial_puts ("yes, ca marche !!!\r\n");
+		serial_puts ("bootstrap finished, starting Trampoline...");
+		serial_puts (" GO!\n");
     StartOS(OSDEFAULTAPPMODE);
     return 0;
 }
 FUNC(void, AUTOMATIC) ProtectionHook(StatusType error)
 {
-	trace++;
 }
 
 FUNC(void, AUTOMATIC) ErrorHook(StatusType error)
@@ -39,28 +22,31 @@ DeclareTask (t2);
 
 TASK(t1)
 {
-	u32 *ptr;
-
-	var_t1 = 1;
- 	var_t2 = 3; /* Try to write in the task 2 data section's */
-	ptr = &CONST_TO_WRITE;
-	*ptr = 18;
+	serial_puts ("t1 starts t2\n");
 	ActivateTask (t2);
+	serial_puts ("t1 will terminate\n");
 	TerminateTask();
 }
 
 TASK(t4)
 {
+	serial_puts ("t4 says hello\n");
 	TerminateTask ();
 }
 
 TASK(t3)
 {
+	serial_puts ("t3 starts t4\n");
+	ActivateTask (t4);
+	serial_puts ("t3 will terminate\n");
 	TerminateTask ();
 }
 
 TASK(t2)
 {
+	serial_puts ("t2 starts t3\n");
+	ActivateTask (t3);
+	serial_puts ("t2 will terminate\n");
 	TerminateTask ();
 }
 
