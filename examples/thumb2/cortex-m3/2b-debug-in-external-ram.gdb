bd04270c590d3a03b84e1012714f41a9bcb782d8
pacco <pacco@e876e4f3-65f1-0310-8132-c7840cfc2588>
2010-10-27 17:39:06 +0000

    example/thumb2

---
+++ examples/thumb2/cortex-m3/2b-debug-in-external-ram.gdb
@@ -0,0 +1,12 @@
+target remote localhost:3333
+monitor arm7_9 force_hw_bkpts enable
+monitor wait_halt
+monitor mww 0xE002C014 0x0F804924
+monitor mdw 0xE002C014
+monitor mww 0xFFE00004 0x20000400
+monitor mdw 0xFFE00004
+file lonely_exe
+load
+monitor mww 0xE01FC040 2
+monitor mdw 0xE01FC040
+monitor soft_reset_halt
---
+++ examples/thumb2/cortex-m3/lonely.c
@@ -0,0 +1,122 @@
+/* Libraries */
+
+#include "tpl_os.h"
+//#include "LPC22xx.h"
+//#include "timer.h"
+//#include "lcd.h"
+
+/* Main */
+
+#define APP_Task_task1_START_SEC_CODE
+#include "tpl_memmap.h"
+FUNC(int, OS_APPL_CODE) main(void)
+{            
+    StartOS(OSDEFAULTAPPMODE);
+    return 0;
+}
+#define APP_Task_task1_STOP_SEC_CODE
+#include "tpl_memmap.h"
+
+/* Vars */
+#define OS_START_SEC_VAR_32BIT
+#include "tpl_memmap.h"
+VAR(int, OS_VAR) switch_count = 0;
+VAR(TaskType, OS_VAR) new_task;
+#define OS_STOP_SEC_VAR_32BIT
+#include "tpl_memmap.h"
+
+#define APP_Task_task1_START_SEC_CONST_32BIT
+#include "tpl_memmap.h"
+CONST(int, AUTOMATIC) toto = 0;
+#define APP_Task_task1_STOP_SEC_CONST_32BIT
+#include "tpl_memmap.h"
+
+#define APP_Task_task1_START_SEC_VAR_32BIT
+#include "tpl_memmap.h"
+VAR(int, AUTOMATIC) titi = 0;
+#define APP_Task_task1_STOP_SEC_VAR_32BIT
+#include "tpl_memmap.h"
+
+#define APP_Task_task2_START_SEC_VAR_32BIT
+#include "tpl_memmap.h"
+VAR(int, AUTOMATIC) lcdstate = 0;
+#define APP_Task_task2_STOP_SEC_VAR_32BIT
+#include "tpl_memmap.h"
+
+/* Application */
+
+DeclareTask(task1);
+DeclareTask(task2);
+DeclareTask(task3);
+
+#define APP_Task_task1_START_SEC_CODE
+#include "tpl_memmap.h"
+TASK(task1)
+{
+    //LCDSendTxt("1");
+    
+    ActivateTask(task3);
+    
+    //LCDSendTxt("3");
+        
+    TerminateTask();
+    
+}
+#define APP_Task_task1_STOP_SEC_CODE
+#include "tpl_memmap.h"
+
+
+#define APP_Task_task2_START_SEC_CODE
+#include "tpl_memmap.h"
+TASK(task2)
+{
+    /*if (lcdstate == 0)
+    {
+        IO0SET = 0x00000400 ; //led on
+        lcdstate = 1;
+    }
+    else if (lcdstate == 1)
+    {
+        IO0CLR = 0x00000400 ; //led off
+        lcdstate = 0;
+    }*/
+    TerminateTask();
+ 
+}
+#define APP_Task_task2_STOP_SEC_CODE
+#include "tpl_memmap.h"
+
+
+#define APP_Task_task3_START_SEC_CODE
+#include "tpl_memmap.h"
+TASK(task3)
+{
+  //LCDSendTxt("2");
+  TerminateTask();    
+    
+}
+#define APP_Task_task3_STOP_SEC_CODE
+#include "tpl_memmap.h"
+
+
+/*
+ #define APP_ISR_isr_button1_START_SEC_CODE
+#include "tpl_memmap.h"
+ISR(isr_button1)
+{
+  LCDSendCommand(CLR_DISP);
+  LCDSendTxt("B1 pressed");
+}
+#define APP_ISR_isr_button1_STOP_SEC_CODE
+#include "tpl_memmap.h"
+
+#define APP_ISR_isr_button2_START_SEC_CODE
+#include "tpl_memmap.h"
+ISR(isr_button2)
+{
+  LCDSendCommand(CLR_DISP);
+  LCDSendTxt("B2 pressed");
+}
+#define APP_ISR_isr_button2_STOP_SEC_CODE
+#include "tpl_memmap.h"
+*/
