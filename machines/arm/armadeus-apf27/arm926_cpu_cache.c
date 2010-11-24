389b76f20e7259b9ca5434fe62cfdf2ca1e33b4c
ilias <ilias@e876e4f3-65f1-0310-8132-c7840cfc2588>
2010-11-24 13:53:27 +0000

    minor changes: tabs -> spaces

--- machines/arm/armadeus-apf27/arm926_cpu_cache.c
+++ machines/arm/armadeus-apf27/arm926_cpu_cache.c
@@ -28,20 +28,20 @@
 #if WITH_CPU_CACHE == NO
 FUNC (void, OS_CODE) arm926_cache_off ()
 {
-	__asm__ ("MRC p15, 0, r0, c1, c0, 0 \n"
-	         "BIC r0, r0, #0b1000000000000 \n"
-	         "BIC r0, r0, #0b0000000000100 \n"
-					 "MCR p15, 0, r0, c1, c0, 0");
+  __asm__ ("MRC p15, 0, r0, c1, c0, 0 \n"
+           "BIC r0, r0, #0b1000000000000 \n"
+           "BIC r0, r0, #0b0000000000100 \n"
+           "MCR p15, 0, r0, c1, c0, 0");
 }
 
 #else /* WITH_CPU_CACHE == YES */
 
 FUNC (void, OS_CODE) arm926_cache_on ()
 {
-	__asm__ ("MRC p15, 0, r0, c1, c0, 0 \n"
-	         "ORR r0, r0, #0b1000000000000 \n"
-	         "ORR r0, r0, #0b0000000000100 \n"
-					 "MCR p15, 0, r0, c1, c0, 0")\n
+  __asm__ ("MRC p15, 0, r0, c1, c0, 0 \n"
+           "ORR r0, r0, #0b1000000000000 \n"
+           "ORR r0, r0, #0b0000000000100 \n"
+           "MCR p15, 0, r0, c1, c0, 0")\n
 }
 
 FUNC (void, OS_CODE) arm926_cache_invalidate ()
