84d224a14ac4f81eca2ac1e9243716741643ae59
jlb <jlb@e876e4f3-65f1-0310-8132-c7840cfc2588>
2012-06-19 13:20:43 +0000

    posix example

--- trunk/examples/posix/lab1/lab1.c
+++ trunk/examples/posix/lab1/lab1.c
@@ -45,49 +45,6 @@ DeclareEvent(evt_t3);
 DeclareTask(task_t3);
 DeclareTask(server);
 
-/*TASK(task_0)
-{
-    EventMaskType event_got;
-    TaskType id;
-    GetTaskID(&id);
-    WaitEvent(evt_0);
-    printf("I am task_0, my id is %d\n",id);
-    ClearEvent(evt_0);
-	TerminateTask();
-        
-}
-TASK(task_1)
-{
-    EventMaskType event_got;
-    TaskType id;
-    GetTaskID(&id);
-    WaitEvent(evt_1);
-    printf("I am task_1, my id is %d\n",id);
-    ClearEvent(evt_1);
-	TerminateTask();
-        
-}
-TASK(a_task)
-{
-    TaskType id;
-    GetTaskID(&id);
-    
-    printf("I am a task, my id is %d\n",id);
-    ActivateTask(task_0);
-    ActivateTask(task_1);
-    SetEvent(task_0,evt_0);
-    SetEvent(task_1,evt_1);
-    //ShutdownOS(E_OK);
-    TerminateTask();
-    //StatusType result= ChainTask(task_0);
-   
-//    if (result==E_OS_ID)
-//    printf("The error is E_OS_ID and value is %d\n",result);
-//    else if (result==E_OS_LIMIT)
-//    printf("The error is E_OS_LIMIT and value is %d\n",result);
-//    else
-//    printf("There is no error");
-}*/
 TASK(server)
 {
     EventMaskType event_got;
@@ -97,7 +54,6 @@ TASK(server)
     ActivateTask(task_t2);
     ActivateTask(task_t3); 
     while(1){
-//      printrl();
       printf("\nI am a task, my id is %d\n",id);
       WaitEvent(evt_t1 | evt_t2 | evt_t3);
       GetEvent(server, &event_got);
@@ -120,11 +76,8 @@ TASK(server)
         ActivateTask(task_t3);
       }
     }
-
-    //ShutdownOS(E_OK);
-    //TerminateTask();
-
 }
+
 TASK(task_t1)
 {
     
