ebe2536a8be47bacdff5c21947919181bb878625
Jean-Luc Béchennec <jlbirccyn@users.noreply.github.com>
2012-11-06 09:55:52 +0000

    periodic example back in posix, INSTALL updated

---
+++ examples/posix/periodic/periodic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "tpl_os.h"
+
+int main(void)
+{
+    StartOS(OSDEFAULTAPPMODE);
+    return 0;
+}
+
+TASK(my_periodic_task)
+{
+    static int occurence = 0;
+    
+    occurence++;
+    printf("Activation #%d\n",occurence);
+}
+
