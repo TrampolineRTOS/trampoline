16dd5bdaa16441d7a9269d123138502b8f3787a7
mik <mbriday@users.noreply.github.com>
2014-03-20 10:45:47 +0000

    [AVR port update]   * support ATMega2560 (Program Counter has 3 bytes)   * larger stack size for idle task (80 bytes).   * ISR2 not tested!

--- examples/avr/main.c
+++ examples/avr/main.c
@@ -2,17 +2,25 @@
 #include "tpl_app_config.h"
 #include <avr/io.h>
 
-//ISR2 associated to an external interrupt source (Int7 on portE.7)
-//Note: The 'ISR' keyword is ALSO used by gcc to set interrupt handlers.
-//      Be careful to use either:
-//        * the one of 'tpl_os.h' (OSEK-ISR2) or 
-//        * the one of 'avr/interrupt.h' (interrupt handler -> OSEK-ISR1)
+//DeclareAlarm(periodicAl);
+////ISR2 associated to an external interrupt source (Int5 on portE.5)
+////Note: The 'ISR' keyword is ALSO used by gcc to set interrupt handlers.
+////      Be careful to use either:
+////        * the one of 'tpl_os.h' (OSEK-ISR2) or 
+////        * the one of 'avr/interrupt.h' (interrupt handler -> OSEK-ISR1)
 //
-ISR(ISRButton) 
-{
-	PORTA ^= 0x20;
-	CallTerminateISR2();	
-}
+////on Arduino Mega, INT5 is PE.5 -> pin number 4
+//ISR(ISRButton)
+//{
+//	static unsigned int period = 1;
+//	//slow alarm related to task periodicTask
+//	period++;
+//	CancelAlarm(periodicAl);
+//	SetRelAlarm(periodicAl,period,period);
+//	//
+//	PORTB ^= (1<<7); //blink Led on PIN 13
+//	//CallTerminateISR2();	
+//}
 
 //remove trampoline definition of ISR..
 #undef ISR
@@ -21,39 +29,41 @@ ISR(ISRButton)
 
 int main(void)
 {
-	//portB => LEDs
-	PORTB = 0;
-	DDRB  = 0xFF;
+	//portA.7 => pin 29 on Arduino Mega.
+	DDRA  |= (1<<7);
+
+	//portB.7 => pin 13 on Arduino Mega (with Yellow Led)
+	DDRB  |= (1<<7);
 
 	//timer2 => base for SystemCounter.
-	TCCR2A = 7; //start timer, prescaler 1024
+	TCCR2B = 7; //start timer, prescaler 1024
 	TIMSK2 = 1; //interrupt on timer2 overflow
 
-	//init button for ISR
-	DDRE  &= 0x7F;	// bit 7 (port E)  is an input
-	PORTE |= 0x80;	// pullup port E.7
-	EICRB =  0x80;  // rising edge detection on E.7 (INT7)
-	EIMSK |= 0x80;  // config interrupt port E.7 => int7
+//	//init button for ISR
+//	DDRE  &= ~(1<<5);	// bit 5 (port E)  is an input
+//	PORTE |= (1<<5);	// pullup port E.7
+//	EICRB |=  0xC0;     // rising edge detection on E.5 (INT5)
+//	EIMSK |= (1<<5);    // config interrupt port E.5 => int5
 
-	sei();
     StartOS(OSDEFAULTAPPMODE);
     return 0;
 }
+
 TASK(secondTask)
 {
-	PORTB |=2;
+	PORTB |=(1<<7);
 	TerminateTask();
 }
 
 TASK(startTask)
 {
-	PORTB |= 1;
+	PORTA |= (1<<7);
 	ChainTask(secondTask);
 }
 
 TASK(periodicTask)
 {
-	PORTB ^= 0x80;
+	PORTA ^= (1<<7);
 	TerminateTask();
 }
 
