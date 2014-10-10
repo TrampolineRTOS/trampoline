8e88cf57e8cadb26c7c77e6df36698f3b377fc09
mik <mbriday@users.noreply.github.com>
2014-10-10 08:31:36 +0000

    [AVR port] major update of the context switch. Context switch function is 80% faster and save 33 bytes of memory per proc: from 25us to 5.6us on a 16MHz ATMega328P!

--- machines/avr/avr_switch_context.s
+++ machines/avr/avr_switch_context.s
@@ -4,16 +4,14 @@
 
 /**
  * @file avr_switch_context.s
- *
- * @section File informations
- * initial file from Guillaume Forget. Copyright IRCCyN 2008-
- * $Date: 2008-07-29 11:06:28 +0200 (lun., 28 avr. 2008) $
- * $Rev: 500 $
- * $Author: gforget $
- * $URL: https://trampoline.rts-software.org/svn/trunk/os/os_machine/avr/avr_switch_context.s $
- *
+ * initial version Guillaume Forget - 04/2008
+ * major update M. Briday - 10/2014
  */
 
+ /*  first argument is a pointer to the old context -> R25:R24
+  * second argument is a pointer to the new context -> R22:R23
+  * no output
+  */
 .global tpl_switch_context
 .global tpl_switch_context_from_it 
 tpl_switch_context:
@@ -21,84 +19,69 @@ tpl_switch_context_from_it:
 	/* push r16 and the sreg register on the stack */
 	push r16
 	in   r16, _SFR_IO_ADDR(SREG)
-	push r16
+	push r16 //push sreg
 
 	/* verification if the first parameter is equal to 0 (Old_context == NULL)*/
-	ldi r16, 0
-	cp r16, r24
+	ldi r16, 0 /* ldi requires a reg number > 15 */
+	cp r16, r24 //low byte of address
 	breq first_part_equal // if the first 8 bits are null jump to first_part_equal
 	rjmp save_context // else jump to save_context
 
 first_part_equal :
-	cp r16, r25
+	cp r16, r25 //high byte of address
 	breq unsave_context // if the second part is null Old_context == NULL then jump to unsave_context
 	
 	/* we have to save the old context*/
 save_context :
 
 	/* put all register on the stack */
-	push r31
-	push r30
-	push r29
-	push r28
-	push r27
-	push r26
-	push r25	
-	push r24
-	push r23
-	push r22
-	push r21
-	push r20
-	push r19
-	push r18
-	push r17
-	// r16 is pushed at the start of the function
-	push r15
-	push r14
-	push r13
-	push r12
-	push r11
-	push r10
-	push r9
-	push r8
-	push r7
-	push r6
-	push r5
-	push r4
-	push r3
-	push r2
-	push r1
 	push r0
-
+	push r1
+	push r2
+	push r3
+	push r4
+	push r5
+	push r6
+	push r7	
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+	/* push r16 done */
+	push r17	
+	push r18
+	push r19
+	push r20
+	push r21
+	push r22
+	push r23
+	push r24
+	push r25
+	push r26
+	push r27	
+	push r28
+	push r29
+	push r30
+	push r31
 
 	/* save the old stack pointer */
 	/* R24 is a pointer on the old context. */
 	movw r28,r24 // mov R24 to Y.
 	ldd  r30,Y+0 // dereference to access the first field
 	ldd  r31,Y+1
-	in   r26, _SFR_IO_ADDR(SPL) //save the Stack pointer into Z (through X)
+	in   r26, _SFR_IO_ADDR(SPL) //get the actual SP into X
 	in   r27, _SFR_IO_ADDR(SPH)
 
-	adiw r26,0x21 // we add 21 to the old stack pointer because we have done 0x21 pushs 
+//	adiw r26,0x21 // we add 21 to the old stack pointer because we have done 0x21 pushs 
 
-	std  Z+0, r26 //save (updated) SP into the first field of the old context.
+	std  Z+0, r26 //save SP into the first field of the old context.
 	std  Z+1, r27
 
-	// we make a "for" loops for(r24=0;r24<=r25;r24++)
-	ldi  r24,0x00
-	ldi  r25,0x21
-	
-	adiw r30,0x01 //because SP is 16 bits wide.
-
-less1 :
-	pop  r26 // we take a register
-	adiw r30,0x01 // we increment the address
-	std  Z+0,r26 // we put the register on the chart
-
-	inc  r24 // r24++
-	cp   r24,r25 
-	brlt less1 // if r24<=r25 jump to less1
-
+	//push 2 dummy regs, because r0 and sreg have been pushed in all cases
 	push r16
 	push r16
 unsave_context : // we have pushed 2 registers on the stack, we have to pop them
@@ -118,58 +101,44 @@ unsave_context : // we have pushed 2 registers on the stack, we have to pop them
 	out _SFR_IO_ADDR(SPL), r28
 	out _SFR_IO_ADDR(SPH), r29 
 
-	// we make a "for" loops for(r24=0;r24<=r25;r24++)
-	ldi  r24,0x00
-	ldi  r25,0x21
-	adiw r30,0x23 // here we fill the tab by the end
-
-less2 :
-	sbiw r30,0x01 
-	ldd  r26,Z+0 // get the old value of the register from the chart
-	push r26 // push the register
-
-	inc  r24 // r24++
-	cp   r24,r25
-	brlt less2 // if r24<=r25 jump to less2
-
 	/* Get all register from the stack */
-	pop r0
-	pop r1
-	pop r2
-	pop r3
-	pop r4
-	pop r5
-	pop r6
-	pop r7
-	pop r8
-	pop r9
-	pop r10
-	pop r11
-	pop r12
-	pop r13
-	pop r14
-	pop r15
-	// r16 is pop at the end of the function
-	pop r17
-	pop r18
-	pop r19
-	pop r20
-	pop r21
-	pop r22
-	pop r23
-	pop r24
-	pop r25
-	pop r26
-	pop r27
-	pop r28
-	pop r29
-	pop r30
 	pop r31
-
-
+	pop r30
+	pop r29
+	pop r28
+	pop r27
+	pop r26
+	pop r25
+	pop r24
+	pop r23
+	pop r22
+	pop r21
+	pop r20
+	pop r19
+	pop r18
+	pop r17
+	/* pop r16 at the end */
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop r7
+	pop r6
+	pop r5
+	pop r4
+	pop r3
+	pop r2
+	pop r1
+	pop r0
 	/* get sreg register and r16 from the stack */
 	pop r16
 	out _SFR_IO_ADDR(SREG), r16
+	/* and last register */
 	pop r16
 
 	ret
+
