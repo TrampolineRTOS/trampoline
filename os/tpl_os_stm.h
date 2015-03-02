aa69e2e28e9229f041f249baf3b478abc654a70e
audrey <audrey@e876e4f3-65f1-0310-8132-c7840cfc2588>
2015-03-02 16:37:51 +0000

    Premiers essais implémentation STM-HRT

---
+++ os/tpl_os_stm.h
@@ -0,0 +1,43 @@
+/*
+ * @file tpl_os_stm.h
+ *
+ * @section desc File description
+ *
+ * Trampoline STM header file
+ *
+ * @section copyright Copyright
+ *
+ * Trampoline OS
+ *
+ * Trampoline is copyright (c) IRCCyN 2005-2007
+ * Copyright ESEO for function and data structures documentation
+ * Trampoline is protected by the French intellectual property law.
+ *
+ * This software is distributed under the Lesser GNU Public Licence
+ *
+ * @section infos File informations
+ *
+ * $Date: 2015-03-02 16:00:39 +0100 (lun., 02 mars 2015) $
+ * $Rev: 0 $
+ * $Author: audrey $
+ * $URL: http://trampoline.rts-software.org/svn/trunk/os/tpl_os_stm.h $
+ */
+
+#include "tpl_os_types.h"
+#include "tpl_os_task_kernel.h"
+
+#define OS_START_SEC_CODE
+#include "tpl_memmap.h"
+
+/**
+ * @internal
+ *
+ * Displays on screen the message given as an argument
+ *
+ * @param   msg     message to display
+ *
+ * @retval  E_OK        no error
+ *
+ */
+FUNC(StatusType, OS_CODE) tpl_screen_display_service(
+  P2CONST(char, AUTOMATIC, OS_VAR) msg);
