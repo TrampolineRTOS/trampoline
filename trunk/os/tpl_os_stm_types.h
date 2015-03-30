853342579850bfd996add4c5a322662be8e6e0f4
audrey <audrey@e876e4f3-65f1-0310-8132-c7840cfc2588>
2015-03-30 10:17:07 +0000

    Ajout tpl_os_stm_types.h

---
+++ trunk/os/tpl_os_stm_types.h
@@ -0,0 +1,18 @@
+#include "tpl_os_internal_types.h" //pour tpl_core_id
+
+/**
+ * @typedef tpl_stm_object_id
+ *
+ * Object id
+ */
+typedef uint32 tpl_stm_object_id;
+
+/**
+ * @typedef ObjectType
+ *
+ * Object identifier
+ *
+ */
+typedef tpl_stm_object_id  ObjectType;
+
+#define DeclareObject(object_id) extern CONST(ObjectType, AUTOMATIC) object_id
