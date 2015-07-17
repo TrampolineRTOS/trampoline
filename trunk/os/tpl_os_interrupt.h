295b64487f51ea5fddffe310ee695d0114fe51a8
jlb <jlb@e876e4f3-65f1-0310-8132-c7840cfc2588>
2015-07-17 12:11:33 +0000

    removing obsolet files

--- trunk/os/tpl_os_interrupt.h
+++ trunk/os/tpl_os_interrupt.h
@@ -38,68 +38,6 @@
  * pages 54+ of OSEK/VDX 2.2.2 spec
  */
 
-/**
- * Enable all interrupts
- *
- * see paragraph 13.3.2.1 page 54 of OSEK/VDX 2.2.2 spec
- *
- * @see #DisableAllInterrupts
- */
-FUNC(void, OS_CODE) EnableAllInterrupts(void);
-
-
-/**
- * Disable all interrupts
- *
- * see paragraph 13.3.2.2 page 55 of OSEK/VDX 2.2.2 spec
- *
- * @see #EnableAllInterrupts
- */
-FUNC(void, OS_CODE) DisableAllInterrupts(void);
-
-
-/**
- * Resume all interrupts
- *
- * see paragraph 13.3.2.3 page 55 of OSEK/VDX 2.2.2 spec
- *
- * @see #SuspendAllInterrupts
- */
-FUNC(void, OS_CODE) ResumeAllInterrupts(void);
-
-
-/**
- * Suspend all interrupts
- *
- * see paragraph 13.3.2.4 page 56 of OSEK/VDX 2.2.2 spec
- *
- * @see #ResumeAllInterrupts
- */
-FUNC(void, OS_CODE) SuspendAllInterrupts(void);
-
-
-/**
- * Resume category 2 interrupts
- *
- * see paragraph 13.3.2.5 page 56 of OSEK/VDX 2.2.2 spec
- *
- * @see #SuspendOSInterrupts
- */
-FUNC(void, OS_CODE) ResumeOSInterrupts(void);
-
-
-/**
- * Suspend category 2 interrupts
- *
- * see paragraph 13.3.2.6 page 57 of OSEK/VDX 2.2.2 spec
- *
- * @see #ResumeOSInterrupts
- */
-FUNC(void, OS_CODE) SuspendOSInterrupts(void);
-
-#define API_STOP_SEC_CODE
-#include "tpl_memmap.h"
-
 #endif /* TPL_OS_IT_H */
 
 /* End of file tpl_os_it.h */
