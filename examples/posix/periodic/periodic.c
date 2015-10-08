da4c16783a414735dd8e2714b4e2c69197bc23cb
Mikaël Briday <mikael.briday@irccyn.ec-nantes.fr>
2015-10-08 14:34:17 +0200

    [example] add first posix example. Doc needed.

---
+++ examples/posix/periodic/periodic.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "tpl_os.h"
+
+int main(void)
+{
+    StartOS(OSDEFAULTAPPMODE);
+    return 0;
+}
+
+DeclareAlarm(one_second);
+
+TASK(my_periodic_task)
+{
+  static int occurence = 0;
+    
+  occurence++;
+  printf("Activation #%d\n",occurence);
+    
+  TerminateTask();
+}
+
+TASK(stop)
+{
+  CancelAlarm(one_second);
+  printf("Terminaison\n");
+  TerminateTask();
+}
