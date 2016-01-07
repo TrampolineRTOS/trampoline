fdc4e70969f1a6f582efdb4490d1d39f24f7743d
Mikaël Briday <mikael.briday@irccyn.ec-nantes.fr>
2016-01-07 18:08:17 +0100

    start Cortex-M0+ port. Start with the oil config part adapted from Cortex-M4 (interrupt sources, compilation options)

---
+++ examples/cortex/armv6m/samd21/XPlainedPro/blink/README.md
@@ -0,0 +1,21 @@
+|=-----=[ Blink example ]=-----=|
+
+This application is a simple periodic example wich toggles the yellow led of the board.
+Have a look into "blink.oil" file.
+
+The system is based scheduled with a 1ms SysTick "SystemCounter".
+
+The task "blink" toggles the YELLOW (PB.30) led when executed.
+This is not scheduled when the program starts.
+
+This task is activated by the alarm "blink_blink".
+This alarm starts 250ms (ALARMTIME) after "StartOS".
+This alarm has a 250ms (CYCLETIME) period.
+
+Configure the application with
+
+TODO:
+
+`
+goil --target=cortex/armv6-m/samd21/XPlainedPro --templates=../../../../../../goil/templates/ blink.oil
+`
---
+++ examples/cortex/armv6m/samd21/XPlainedPro/blink/blink.c
@@ -0,0 +1,49 @@
+#include "tp.h"
+#include "tpl_os.h"
+
+#define APP_Task_blink_START_SEC_CODE
+#include "tpl_memmap.h"
+FUNC(int, OS_APPL_CODE) main(void)
+{
+  initBoard();
+  StartOS(OSDEFAULTAPPMODE);
+  return 0;
+}
+
+TASK(blink)
+{
+  ledToggle(BLUE);
+  TerminateTask();
+}
+#define APP_Task_blink_STOP_SEC_CODE
+#include "tpl_memmap.h"
+
+#define OS_START_SEC_CODE
+#include "tpl_memmap.h"
+/*
+ *  * This is necessary for ST libraries
+ *   */
+FUNC(void, OS_CODE) assert_failed(uint8_t* file, uint32_t line)
+{
+}
+
+FUNC(void, OS_CODE) PreTaskHook()
+{
+  TaskType task_id = 0;
+  GetTaskID(&task_id);
+  if (task_id == blink) {
+    ledOn(ORANGE);
+  }
+}
+
+FUNC(void, OS_CODE) PostTaskHook()
+{
+  TaskType task_id = 0;
+  GetTaskID(&task_id);
+  if (task_id == blink) {
+    ledOff(ORANGE);
+  }
+}
+#define OS_STOP_SEC_CODE
+#include "tpl_memmap.h"
+
