75ab0dc31df5de1960906be41dac1a017394bd8c
Jean-Luc Béchennec <Jean-Luc.Bechennec@irccyn.ec-nantes.fr>
2017-01-02 16:18:47 +0100

    [Cortex] Improvements and working examples for interrupt handling.

--- machines/cortex/armv7em/stm32f407/tpl_machine_stm32f407.c
+++ machines/cortex/armv7em/stm32f407/tpl_machine_stm32f407.c
@@ -28,14 +28,10 @@
 #include "tpl_os_std_types.h"
 #include "tpl_os_definitions.h"
 #include "tpl_machine.h"
+#include "tpl_cortex_definitions.h"
 
 #define OS_START_SEC_CODE
 #include "tpl_memmap.h"
-
-/*
- * The NVIC in the STM32F407 uses 4 bits to store priorities
- */
-static const uint32 PRIO_BITS_IN_NVIC = 4;
 
 __STATIC_INLINE FUNC(uint32, OS_CODE) tpl_configure_systick(
   CONST(uint32, AUTOMATIC) ticks,
@@ -61,11 +57,11 @@ __STATIC_INLINE FUNC(uint32, OS_CODE) tpl_configure_systick(
 
 FUNC(void, OS_CODE) tpl_set_systick_timer()
 {
-	if (tpl_configure_systick(SystemCoreClock / 1000, 14) != E_OK)
-	{
+  if (tpl_configure_systick(SystemCoreClock / 1000, KERNEL_PRIO_UNSHIFTED) != E_OK)
+  {
     /* Failed to initialize Systick, fall back in an infinite loop */
-		while(1);
-	}
+    while(1);
+  }
 }
 #define OS_STOP_SEC_CODE
 #include "tpl_memmap.h"
