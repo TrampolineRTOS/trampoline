e1668df35acfb1b40ab6807d8fa91ff6a2f68fcc
Jean-Luc Béchennec <Jean-Luc.Bechennec@irccyn.ec-nantes.fr>
2017-01-18 16:30:41 +0100

    [cortex] Added instrumentation in tpl_sc_handler

---
+++ machines/cortex/tpl_fabric_definitions.h
@@ -0,0 +1,15 @@
+/*
+ *
+ */
+
+#ifndef TPL_FABRIC_DEFINITIONS_H
+#define TPL_FABRIC_DEFINITIONS_H
+
+
+#define BIT(n)  (1 << (n))
+
+
+#define AD_REG_OS_INSTRU 0x50000020
+
+#define ID_CALL_HANDLER   BIT(0)
+#endif /* TPL_FABRIC_DEFINITIONS_H */
