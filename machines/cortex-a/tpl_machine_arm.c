TrampolineRTOS/trampoline

commit b1fc96a2b55d0865322b199a461864f00ec072b9

[cortex-a] [NOT TESTED] fix goil access for counters; add dummy tpl_os_enable/disable_interrupts functions; to be continued

machines/cortex-a/tpl_machine_arm.c
@@ -152,6 +152,9 @@ void tpl_disable_interrupts(void)
 
 }
 
+void tpl_os_disable_interrupts(void) {}
+void tpl_os_enable_interrupts(void) {}
+
 /*
  * tpl_init_context initialize a context to prepare a task to run.
  * It sets up the stack and the entry point

goil/templates/code/cortex-a/armv7/bcm2836/counter_call.goilTemplate
@@ -20,10 +20,10 @@ FUNC(void, OS_CODE) tpl_tick_% !interrupt::NAME %()
 {
 %
     do
-      %  tpl_counter_tick(&% !cpt_fct::VALUE %_counter_desc);
+      %  tpl_counter_tick(&% !cpt_fct %_counter_desc);
 %
     after %
-  
+
   if (tpl_kern.need_schedule)
   {
     tpl_schedule_from_running();

commit acb10e4267faeb05c4656f02f1f69b02e012bb7a

Fix compilation issues introduced by b1fc96a2b55d0865322 and 12e6735968f8c656db55ea for rpi2 examples

machines/cortex-a/tpl_machine_arm.c
@@ -152,8 +152,15 @@ void tpl_disable_interrupts(void)
 
 }
 
-void tpl_os_disable_interrupts(void) {}
-void tpl_os_enable_interrupts(void) {}
+void tpl_disable_os_interrupts(void)
+{
+  tpl_disable_interrupts();
+}
+
+void tpl_enable_os_interrupts(void)
+{
+  tpl_enable_interrupts();
+}
 
 /*
  * tpl_init_context initialize a context to prepare a task to run.