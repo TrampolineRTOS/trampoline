4f8f415e75f372f34e0543d6eb7c0f7ac2e0f1f2
Mikaël BRIDAY <mikael.briday@univ-nantes.fr>
2019-06-04 17:50:38 +0200

    [msp430] first version with startup code. WiP

--- machines/msp430/msp430x/tpl_machine.c
+++ machines/msp430/msp430x/tpl_machine.c
@@ -1,7 +1,22 @@
 #include "tpl_machine.h"
 
-void tpl_reset_handler()
+//function that should be defined for msp430...
+
+FUNC(void, OS_CODE) tpl_init_context(
+  CONST(tpl_proc_id, OS_APPL_DATA) proc_id)
 {
-	//TODO: startup
+}
+
+void tpl_disable_interrupts() {}
+void tpl_enable_interrupts() {}
+void tpl_disable_os_interrupts() {}
+void tpl_enable_os_interrupts() {}
+
+void tpl_sc_handler() {}
+
+void idle_function(void)
+{
 	while(1);
 }
+
+void tpl_init_machine() {};
---
+++ machines/msp430/msp430x/tpl_startup.c
@@ -0,0 +1,63 @@
+/**
+ * @section descr File description
+ *
+ * @section copyright Copyright
+ *
+ * Trampoline OS
+ *
+ * Trampoline is copyright (c) IRCCyN 2005+
+ * Copyright DG for function and data structures documentation and THUMB2 port
+ * Trampoline is protected by the French intellectual property law.
+ *
+ * This software is distributed under the Lesser GNU Public Licence
+ *
+ */
+
+#include "tpl_os.h"
+//#include "tpl_compiler.h"
+//#include "tpl_os_interrupt_kernel.h"
+
+extern int main (void);
+
+void tpl_continue_reset_handler(void)
+{
+//---------3- Init .bss section
+  extern unsigned __bss_start__ ;
+  extern unsigned __bss_end__ ;
+  unsigned * p = & __bss_start__ ;
+  while (p != & __bss_end__) {
+    * p = 0 ;
+    p ++ ;
+  }
+//---------4- Init .data section 
+  extern unsigned __data_start ;
+  extern unsigned __data_end ;
+  extern unsigned __data_load_start ;
+  unsigned * pSrc = & __data_load_start ;
+  unsigned * pDest = & __data_start ;
+  while (pDest != & __data_end) {
+    * pDest = * pSrc ;
+    pDest ++ ;
+    pSrc ++ ;
+  }
+
+//---------5- Exec constructors for global variables
+//  extern void (* __constructor_array_start) (void) ;
+//  extern void (* __constructor_array_end) (void) ;
+//  void (** ptr) (void) = & __constructor_array_start ;
+//  while (ptr != & __constructor_array_end) {
+//    (* ptr) () ;
+//    ptr ++ ;
+//  }
+//---------6- Exécuter les routines d'initialisation de la section init_routine_array
+//   extern void (* __init_routine_array_start) (void) ;
+//   extern void (* __init_routine_array_end) (void) ;
+//   ptr = & __init_routine_array_start ;
+//   while (ptr != & __init_routine_array_end) {
+//     (* ptr) () ;
+//     ptr ++ ;
+//   }
+//---------7- Exécuter le programme utilisateur
+  main();
+  while (1) ;
+}
