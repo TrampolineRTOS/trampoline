d2dc3d89cd81ccaecbad9595939bb1d39f10fdf9
Mikaël BRIDAY <mikael.briday@ls2n.fr>
2019-09-13 12:08:55 +0200

    [msp430] asm cleanup (ABI definitions)

--- machines/msp430/msp430x/tpl_asm_definitions.h
+++ machines/msp430/msp430x/tpl_asm_definitions.h
@@ -2,7 +2,7 @@
 #define TPL_ASM_DEFINITIONS_H
 
 /*
- * tpl_kern data structure offsets (for use into assebmler sources)
+ * tpl_kern data structure offsets (for use into assembler sources)
  */
 .equ TPL_KERN_OFFSET_S_RUNNING,     0
 .equ TPL_KERN_OFFSET_S_ELECTED,     2
@@ -12,15 +12,5 @@
 .equ TPL_KERN_OFFSET_ELECTED_ID,    12
 .equ TPL_KERN_OFFSET_NEED_SWITCH,   16
 .equ TPL_KERN_OFFSET_NEED_SCHEDULE, 17
-
-/* operating modes */
-.equ ACTIVE, 0b0000
-.equ LPM0,   0b0001
-.equ LPM1,   0b0101
-.equ LPM2,   0b1001
-.equ LPM3,   0b1101
-.equ LPM4,   0b1111
-.equ LPM35,  0b1111
-.equ LPM45,  0b1111
 
 #endif
--- machines/msp430/msp430x/tpl_assembler.h
+++ machines/msp430/msp430x/tpl_assembler.h
@@ -15,24 +15,23 @@
 #define R9 r9
 #define R10 r10
 #define R11 r11
+//R12 behavior depends on the ABI flavor
+#define R13 r13
+#define R14 r14
+//R15 behavior depends on the ABI flavor
 
 #if __GXX_ABI_VERSION == 1002
-/* MSPGCC ABI */
+	/* MSPGCC ABI */
     #define MSPGCC_ABI
     #define REG_SID r12
-    #define R13 r13
-    #define R14 r14
     #define REG_RETARG r15
 
 #elif __GXX_ABI_VERSION == 1011
     /* GCC compiler for MSP ABI */
     #define GCCFORMSP_ABI
     #define REG_RETARG r12
-    #define R13 r13
-    #define R14 r14
     #define REG_SID r15
-
-else
+#else
     #error "Unsupported ABI"
 #endif
 
