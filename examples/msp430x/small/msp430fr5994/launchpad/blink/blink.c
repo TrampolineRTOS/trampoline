4291178080887a98ee7fb113daf72f262c64da33
Jean-Luc Béchennec <Jean-Luc.Bechennec@irccyn.ec-nantes.fr>
2019-10-18 19:21:05 +0200

    MSP430FR5994 wip

--- examples/msp430x/small/msp430fr5994/launchpad/blink/blink.c
+++ examples/msp430x/small/msp430fr5994/launchpad/blink/blink.c
@@ -9,16 +9,16 @@ FUNC(int, OS_APPL_CODE) main(void)
 	// Disable the GPIO power-on default high-impedance mode
 	// to activate previously configured port settings
 	PM5CTL0 &= ~LOCKLPM5;
-  //set GPIO P1.0 (LED2) as an output
-  //set GPIO P1.2 as an output
-	P1DIR = 0x05;
+  //set GPIO P1.0 (LED1) as an output
+	P1DIR = 0x01;
+
 	StartOS(OSDEFAULTAPPMODE);
 	return 0;
 }
 
 TASK(blink)
 {
-	P1OUT ^= 0x05; //toggle GPIO P1.0 (Led2)
+	P1OUT ^= 0x01; //toggle GPIO P1.0 (LED1)
 	TerminateTask();
 }
 
