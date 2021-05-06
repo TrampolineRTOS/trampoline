5f8615be6e03702de0e208bfcdaccf224c94e600
Mikaël BRIDAY <mikael.briday@ec-nantes.fr>
2021-05-06 17:52:59 +0200

    [coro board] update CS for TFT - modified hw

--- machines/cortex/armv7em/stm32f303/coroLabBoard/tft/Adafruit-GFX-Library/Adafruit_SPITFT_Macros.h
+++ machines/cortex/armv7em/stm32f303/coroLabBoard/tft/Adafruit-GFX-Library/Adafruit_SPITFT_Macros.h
@@ -8,8 +8,8 @@
 
 #define SPI_DC_HIGH()           {GPIOA->BSRR = GPIO_BSRR_BS_12;}
 #define SPI_DC_LOW()            {GPIOA->BSRR = GPIO_BSRR_BR_12;}
-#define SPI_CS_HIGH()           {GPIOA->BSRR = GPIO_BSRR_BS_2;}
-#define SPI_CS_LOW()            {GPIOA->BSRR = GPIO_BSRR_BR_2;}
+#define SPI_CS_HIGH()           {GPIOA->BSRR = GPIO_BSRR_BS_4;}
+#define SPI_CS_LOW()            {GPIOA->BSRR = GPIO_BSRR_BR_4;}
 
 /*
  * Software SPI Macros
