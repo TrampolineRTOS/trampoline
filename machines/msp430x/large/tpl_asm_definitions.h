45e9fd4e0e05801f5d286ccf9d91fc85ff1d028d
antoine-bernabeu <antoine.bernabeu@zoho.com>
2022-02-01 08:57:54 +0100

    added debug assembly, sc_handler, and change in offset for tpl_kern

--- machines/msp430x/large/tpl_asm_definitions.h
+++ machines/msp430x/large/tpl_asm_definitions.h
@@ -5,13 +5,13 @@
  * tpl_kern data structure offsets (for use into assembler sources)
  */
 .equ TPL_KERN_OFFSET_S_RUNNING,     0
-.equ TPL_KERN_OFFSET_S_ELECTED,     2
-.equ TPL_KERN_OFFSET_RUNNING,       4
-.equ TPL_KERN_OFFSET_ELECTED,       6
-.equ TPL_KERN_OFFSET_RUNNING_ID,    8
-.equ TPL_KERN_OFFSET_ELECTED_ID,    12
-.equ TPL_KERN_OFFSET_NEED_SWITCH,   16
-.equ TPL_KERN_OFFSET_NEED_SCHEDULE, 17
+.equ TPL_KERN_OFFSET_S_ELECTED,     4
+.equ TPL_KERN_OFFSET_RUNNING,       8
+.equ TPL_KERN_OFFSET_ELECTED,       12
+.equ TPL_KERN_OFFSET_RUNNING_ID,    16
+.equ TPL_KERN_OFFSET_ELECTED_ID,    20
+.equ TPL_KERN_OFFSET_NEED_SWITCH,   24
+.equ TPL_KERN_OFFSET_NEED_SCHEDULE, 25
 
 /* Init values for various fields of tpl_kern */
 .equ NO_NEED_SWITCH_NOR_SCHEDULE,   0
--- machines/msp430x/large/tpl_machine_msp430x.h
+++ machines/msp430x/large/tpl_machine_msp430x.h
@@ -7,13 +7,13 @@
 #include "tpl_os_custom_types.h"
 
 #define TPL_KERN_OFFSET_S_RUNNING     0
-#define TPL_KERN_OFFSET_S_ELECTED     2
-#define TPL_KERN_OFFSET_RUNNING       4
-#define TPL_KERN_OFFSET_ELECTED       6
-#define TPL_KERN_OFFSET_RUNNING_ID    8
-#define TPL_KERN_OFFSET_ELECTED_ID    12
-#define TPL_KERN_OFFSET_NEED_SWITCH   16
-#define TPL_KERN_OFFSET_NEED_SCHEDULE 17
+#define TPL_KERN_OFFSET_S_ELECTED     4
+#define TPL_KERN_OFFSET_RUNNING       8
+#define TPL_KERN_OFFSET_ELECTED       12
+#define TPL_KERN_OFFSET_RUNNING_ID    16
+#define TPL_KERN_OFFSET_ELECTED_ID    20
+#define TPL_KERN_OFFSET_NEED_SWITCH   24
+#define TPL_KERN_OFFSET_NEED_SCHEDULE 25
 
 //we save 14 registers: from top
 // Each register uses 32 bits (20 bits in hardware), but SR and SP are merged.
