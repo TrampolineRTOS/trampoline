716582cb346d6fd68314c8907c65ba69fb5e3ace
antoine-bernabeu <antoine.bernabeu@zoho.com>
2022-08-29 16:48:17 +0200

    WiP sequence, start sequence data structure

---
+++ examples/msp430x/large/msp430fr5994/launchpad/sequence/sequence.c
@@ -0,0 +1,68 @@
+#include "tpl_os.h"
+#include "msp430.h"
+#include <stdint.h>
+
+#define APP_COMMON_START_SEC_CODE
+#include "tpl_memmap.h"
+
+FUNC(int, OS_APPL_CODE) main(void)
+{
+	// Disable the GPIO power-on default high-impedance mode
+	// to activate previously configured port settings
+	PM5CTL0 &= ~LOCKLPM5;
+  	//set GPIO P1.0 (LED1) as an output
+	P1DIR |= BIT0;
+	StartOS(OSDEFAULTAPPMODE);
+	return 0;
+}
+#define APP_COMMON_STOP_SEC_CODE
+#include "tpl_memmap.h"
+
+#define APP_Task_blink_START_SEC_CODE
+#include "tpl_memmap.h"
+
+TASK(blink)
+{
+	P1OUT ^= BIT0; //toggle GPIO P1.0 (LED1)
+	TerminateTask();
+}
+
+#define APP_Task_blink_STOP_SEC_CODE
+#include "tpl_memmap.h"
+
+#define APP_Task_fibo_START_SEC_CODE
+#include "tpl_memmap.h"
+
+TASK(fibo) {
+    // TaskType runningTaskID ;
+    // GetTaskID(&runningTaskID);
+    // tpl_serial_print_string("fibo: ");
+    // tpl_serial_print_int(runningTaskID, 0);
+    // tpl_serial_print_string("\n");
+
+    static uint16 l_value_ind_0 = 0;
+    static uint16 l_value_ind_1 = 1;
+    uint16 l_temp;
+
+    // /* Print last value of fibo */
+    // tpl_serial_print_string("fibo :");
+    // tpl_serial_print_int(l_value_ind_0,0);
+    // tpl_serial_print_string("\r\n");
+
+    /* Compute next value of fibo */
+    if (l_value_ind_0 > 28000) {
+        /* No overflow, restart fibo */
+        l_value_ind_0 = 0;
+        l_value_ind_1 = 1;
+    } 
+    else {
+        /* compute fibo */
+        l_temp = l_value_ind_0 + l_value_ind_1;
+        l_value_ind_0 = l_value_ind_1;
+        l_value_ind_1 = l_temp;
+    }
+  TerminateTask();
+}
+
+#define APP_Task_fibo_STOP_SEC_CODE
+#include "tpl_memmap.h"
