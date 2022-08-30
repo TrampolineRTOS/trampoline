9910d3ee865bb4166fcaa1639d7fe49ad5c53301
antoine-bernabeu <antoine.bernabeu@zoho.com>
2022-08-30 11:25:40 +0200

    header sequence kernel file

--- os/tpl_sequence_kernel.h
+++ os/tpl_sequence_kernel.h
@@ -27,10 +27,31 @@
 #ifndef TPL_OS_SEQUENCE_H
 #define TPL_OS_SEQUENCE_H
 
+#include "tpl_compiler.h"
+#include "tpl_os_internal_types.h"
+
+struct TPL_SEQUENCE{
+    CONST(uint32, TYPEDEF) energy;
+    CONST(uint8, TYPEDEF) next_state;
+    CONST(uint8, TYPEDEF) current_state; 
+    CONST(uint8, TYPEDEF) trace[];
+};
+
+typedef struct TPL_SEQUENCE tpl_sequence;
+
+#define OS_START_SEC_CONST_UNSPECIFIED
+#include "tpl_memmap.h"
+
+extern CONSTP2VAR(tpl_sequence, AUTOMATIC, OS_APPL_DATA)
+    tpl_sequence_table[2];
+
+#define OS_STOP_SEC_CONST_UNSPECIFIED
+#include "tpl_memmap.h"
+
 #define OS_START_SEC_CODE
 #include "tpl_memmap.h"
 
-FUNC(void, OS_CODE) tpl_activate_sequence_os_service(void);
+FUNC(void, OS_CODE) tpl_start_os_sequence_service(CONST(tpl_application_mode, AUTOMATIC) mode);
 
 #define OS_STOP_SEC_CODE
 #include "tpl_memmap.h"
