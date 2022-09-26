1fbe027458d111f473c7292d96486b608855148d
Mikaël BRIDAY <mikael.briday@ec-nantes.fr>
2022-09-26 16:02:32 +0200

    [msp430] update blink…

--- examples/msp430x/large/msp430fr5994/launchpad/blink/blink.c
+++ examples/msp430x/large/msp430fr5994/launchpad/blink/blink.c
@@ -1,23 +1,21 @@
 #include "tpl_os.h"
 #include "msp430.h"
+#include "leds.h"
 
 #define APP_Task_blink_START_SEC_CODE
 #include "tpl_memmap.h"
 
+
 FUNC(int, OS_APPL_CODE) main(void)
 {
-	// Disable the GPIO power-on default high-impedance mode
-	// to activate previously configured port settings
-	PM5CTL0 &= ~LOCKLPM5;
-  //set GPIO P1.0 (LED1) as an output
-	P1DIR = 0x01;
+	ledInit();
 	StartOS(OSDEFAULTAPPMODE);
 	return 0;
 }
 
 TASK(blink)
 {
-	P1OUT ^= 0x01; //toggle GPIO P1.0 (LED1)
+	ledToggle(LED1);
 	TerminateTask();
 }
 
