c0472369753ad3eb560e3e62fa140e6c4c60d49f
antoine-bernabeu <antoine.bernabeu@zoho.com>
2022-11-10 09:09:11 +0100

    test running timer example

--- examples/msp430x/large/msp430fr5994/launchpad/blink/blink.c
+++ examples/msp430x/large/msp430fr5994/launchpad/blink/blink.c
@@ -9,6 +9,10 @@
 FUNC(int, OS_APPL_CODE) main(void)
 {
 	ledInit();
+
+	// Setup free running timer
+	TA0CCTL0 = CCIE;
+	TA0CTL = TASSEL__ACLK | MC__CONTINOUS | TACLR;
 	StartOS(OSDEFAULTAPPMODE);
 	return 0;
 }
@@ -20,4 +24,15 @@ TASK(blink)
 }
 
 #define APP_Task_blink_STOP_SEC_CODE
+#include "tpl_memmap.h"
+
+#define APP_ISR_timerA_START_SEC_CODE
+#include "tpl_memmap.h"
+
+ISR(timerA)
+{
+	ledToggle(LED2);
+}
+
+#define APP_ISR_timerA_STOP_SEC_CODE
 #include "tpl_memmap.h"
