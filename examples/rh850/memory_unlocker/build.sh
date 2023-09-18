a775a108702f630a4cecac10d9368c6fb3f31ecd
Adrien Ricciardi <aricciardi@baylibre.com>
2023-09-18 10:44:34 +0200

    examples: rh850: memory_unlocker: Added the flashable firmware generation steps.
    The 'build.sh' script now also generates the '_build/App_CDD_ICCOM_S4_Sample_G4MH.srec'
    firmware to provide to the Flash_Tools 'burn.sh' script.
    
    Signed-off-by: Adrien Ricciardi <aricciardi@baylibre.com>

--- examples/rh850/memory_unlocker/build.sh
+++ examples/rh850/memory_unlocker/build.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 
-#stop on errors
 set -e
 
-if [[ ! -d "_build" ]]
-then
-    mkdir _build
-fi
+PROGRAM_NAME=memory_unlocker
+FLASH_TOOL_PROGRAM_NAME=App_CDD_ICCOM_S4_Sample_G4MH
 
 echo "*** Run Goil ***"
-goil --target=rh850/g4mh --templates=../../../goil/templates/ memory_unlocker.oil
-cd _build
+mkdir -p _build
+goil --target=rh850/g4mh --templates=../../../goil/templates/ ${PROGRAM_NAME}.oil
+
 echo "*** Run CMake ***"
-cmake -G "Unix Makefiles" -D CMAKE_TOOLCHAIN_FILE=../memory_unlocker/compiler.cmake ..
+cd _build
+cmake -G "Unix Makefiles" -D CMAKE_TOOLCHAIN_FILE=../${PROGRAM_NAME}/compiler.cmake ..
+
 echo "*** Run Make ***"
 make
+
+# Create the S-Record file to provide to Flash_Tool to set the G4MH core default firmware on board boot
+# The flashing procedure expects 32-bit addresses, so use S3 records instead of S1
+objcopy -O srec --srec-forceS3 ${PROGRAM_NAME}_exe.abs ${FLASH_TOOL_PROGRAM_NAME}.s3
+# Add a specific header (it has been downloaded here : https://raw.githubusercontent.com/renesas-rcar/whitebox-sdk/v4.x/mcu/G4MH_Head.srec)
+rlink ../G4MH_Head.srec ${FLASH_TOOL_PROGRAM_NAME}.s3 -FOrm=SType -OUtput=${FLASH_TOOL_PROGRAM_NAME}.srec
+rm -f ${FLASH_TOOL_PROGRAM_NAME}.s3
