4f8e93878189a8a240e736598d04ff7160f3d32e
Jean-Luc Béchennec <jean-luc.bechennec@ls2n.fr>
2023-10-16 18:28:06 +0200

    [OS][POSIX][COM][AUTOSAR] Fix missing void in argumentless function, it seems to be required now

--- tests/machines/common/commonTestConfig.c
+++ tests/machines/common/commonTestConfig.c
@@ -22,7 +22,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
  * @section infos File informations
  *
@@ -36,22 +37,22 @@
 #include "tpl_dow.h"
 
 #if WITH_AUTOSAR == YES
-  #include "Os.h"
+#include "Os.h"
 #else
-  #include "tpl_os.h"
+#include "tpl_os.h"
 #endif
 
 #if ALARM_COUNT > 0
-# include "tpl_os_alarm_kernel.h"
-# include "tpl_os_timeobj_kernel.h"
+#include "tpl_os_alarm_kernel.h"
+#include "tpl_os_timeobj_kernel.h"
 #endif
 
 #if ALARM_COUNT > 0
 /* ----------------------------------------------------------------------------
  *  ALARMS UTILS
  */
-extern CONSTP2VAR(tpl_time_obj, AUTOMATIC, OS_APPL_DATA)
-tpl_alarm_table[ALARM_COUNT];
+extern CONSTP2VAR(tpl_time_obj, AUTOMATIC,
+                  OS_APPL_DATA) tpl_alarm_table[ALARM_COUNT];
 
 /*
  * @def WaitActivationPeriodicAlarm
@@ -61,16 +62,18 @@ tpl_alarm_table[ALARM_COUNT];
  * @param Alarm Alarm id
  *
  */
-void WaitActivationPeriodicAlarm(AlarmType Alarm){
+void WaitActivationPeriodicAlarm(AlarmType Alarm)
+{
   uint32 temp, result_inst_;
   TickType result_inst_tt;
   result_inst_tt = 0;
-  do{
+  do
+  {
     temp = result_inst_tt;
-    result_inst_ = GetAlarm(Alarm,&result_inst_tt);
-    /* for debug stdimpl_print("periodic alarm : ticks = %u (temp = %u)\n",result_inst_tt,temp); */
-  }while( (temp >= result_inst_tt) || (temp==(0)) );
-
+    result_inst_ = GetAlarm(Alarm, &result_inst_tt);
+    /* for debug stdimpl_print("periodic alarm : ticks = %u (temp =
+     * %u)\n",result_inst_tt,temp); */
+  } while ((temp >= result_inst_tt) || (temp == (0)));
 }
 
 /*
@@ -81,19 +84,25 @@ void WaitActivationPeriodicAlarm(AlarmType Alarm){
  * @param Alarm Alarm id
  *
  */
-void WaitActivationOneShotAlarm(AlarmType Alarm){
+void WaitActivationOneShotAlarm(AlarmType Alarm)
+{
 
   int result_inst_;
   TickType result_inst_tt;
 
-  do{
-    GetAlarm(Alarm,&result_inst_tt);
-    /* for debug stdimpl_print("one shot alarm : ticks = %lu\n",result_inst_tt); */
-  }while((SetRelAlarm(Alarm, tpl_alarm_table[Alarm]->stat_part->counter->max_allowed_value , 0) == E_OS_STATE));
+  do
+  {
+    GetAlarm(Alarm, &result_inst_tt);
+    /* for debug stdimpl_print("one shot alarm : ticks = %lu\n",result_inst_tt);
+     */
+  } while ((
+      SetRelAlarm(Alarm,
+                  tpl_alarm_table[Alarm]->stat_part->counter->max_allowed_value,
+                  0) == E_OS_STATE));
 
-  result_inst_ = CancelAlarm(Alarm); /* Cancel the previous alarm usefull only for the test */
+  result_inst_ = CancelAlarm(
+      Alarm); /* Cancel the previous alarm usefull only for the test */
   TEST_ASSERT_EQUAL_INT(E_OK, result_inst_);
-
 }
 
 #endif /* ALARM_COUNT > 0 */
@@ -103,8 +112,8 @@ void WaitActivationOneShotAlarm(AlarmType Alarm){
  *  COUNTERS UTILS
  */
 
-extern CONSTP2VAR(tpl_counter, OS_VAR, OS_APPL_DATA)
-tpl_counter_table[COUNTER_COUNT];
+extern CONSTP2VAR(tpl_counter, OS_VAR,
+                  OS_APPL_DATA) tpl_counter_table[COUNTER_COUNT];
 
 /*
  * @def WaitCounterDeltaValue
@@ -116,7 +125,8 @@ tpl_counter_table[COUNTER_COUNT];
  * @param delta_value Delta value to wait to the counter #Counter
  *
  */
-void WaitCounterDeltaValue(CounterType Counter, TickType delta_value){
+void WaitCounterDeltaValue(CounterType Counter, TickType delta_value)
+{
 
   StatusType status_was;
   TickType value_was, value_exp;
@@ -124,18 +134,22 @@ void WaitCounterDeltaValue(CounterType Counter, TickType delta_value){
   /* if delta_value > OSMAXALLOWEDVALUE */
   DOW_ASSERT(delta_value <= tpl_counter_table[Counter]->max_allowed_value);
 
-  /* find the expected value (value_exp) from the actual value (value_was) and delta value (delta_value) */
+  /* find the expected value (value_exp) from the actual value (value_was) and
+   * delta value (delta_value) */
   GetCounterValue(Counter, &value_was);
   value_exp = value_was + delta_value;
 
-  /* if value_exp is higher than OSMAXALLOXEDVALUE, do value_exp - OSMAXALLOWEDVALUE */
-  if(value_exp > tpl_counter_table[Counter]->max_allowed_value){
+  /* if value_exp is higher than OSMAXALLOXEDVALUE, do value_exp -
+   * OSMAXALLOWEDVALUE */
+  if (value_exp > tpl_counter_table[Counter]->max_allowed_value)
+  {
     value_exp -= (tpl_counter_table[Counter]->max_allowed_value + 1);
   }
 
-  do{
+  do
+  {
     status_was = GetCounterValue(Counter, &value_was);
-  }while(value_was != value_exp);
+  } while (value_was != value_exp);
 }
 #endif /* COUNTER_COUNT > 0 && WITH_AUTOSAR == YES */
 
@@ -152,10 +166,10 @@ void WaitCounterDeltaValue(CounterType Counter, TickType delta_value){
 tpl_core_id syncAllCores_maxCores[SPINLOCK_COUNT];
 uint8 syncAllCores_initialized = 0;
 
-void SyncAllCores_Init()
+void SyncAllCores_Init(void)
 {
   uint8 i;
-  for(i = 0; i < SPINLOCK_COUNT; i++)
+  for (i = 0; i < SPINLOCK_COUNT; i++)
   {
     syncAllCores_maxCores[i] = NUMBER_OF_CORES;
   }
@@ -166,29 +180,30 @@ void SyncAllCores_Init()
 void SyncAllCores(SpinlockIdType spinlock)
 {
   StatusType retval;
-  if(!syncAllCores_initialized)
+  if (!syncAllCores_initialized)
   {
     addFailure("SyncAllCores called without init.\n", __LINE__, __FILE__);
     return;
   }
 
   retval = GetSpinlock(spinlock);
-  if(retval != E_OK)
+  if (retval != E_OK)
   {
     addFailure("Cannot get spinlock.\n", __LINE__, __FILE__);
     return;
   }
   syncAllCores_maxCores[spinlock] = syncAllCores_maxCores[spinlock] - 1;
   retval = ReleaseSpinlock(spinlock);
-  if(retval != E_OK)
+  if (retval != E_OK)
   {
     addFailure("Cannot release spinlock.\n", __LINE__, __FILE__);
     return;
   }
-  while(syncAllCores_maxCores[spinlock]);
+  while (syncAllCores_maxCores[spinlock])
+    ;
 }
 #else
-void SyncAllCores_Init()
+void SyncAllCores_Init(void)
 {
 }
 #endif /* (NUMBER_OF_CORES > 1) && (SPINLOCK_COUNT > 0) */
