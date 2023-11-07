06080308c14aa8fef9743ab4ad5c4bfecfe769d1
Jean-Luc Béchennec <jean-luc.bechennec@ls2n.fr>
2023-11-07 18:04:08 +0100

    [POSIX][EXAMPLES] Fix #125

--- examples/posix/ioc/ioc.c
+++ examples/posix/ioc/ioc.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
 #include "Os.h" // TODO: check this include
+#include <stdio.h>
 
 uint32 rcv_message = 0;
 uint32 snd_message = 0;
 
 int main(void)
 {
-    StartOS(OSDEFAULTAPPMODE);
-    return 0;
+  StartOS(OSDEFAULTAPPMODE);
+  return 0;
 }
 
 DeclareAlarm(one_second);
@@ -20,7 +20,7 @@ TASK(receiver)
 #elif IOC_QUEUED_COUNT > 0
   IocReceive_my_ioc_name(&rcv_message);
 #endif
-  printf("Receiver Task Reads : #%d\r\n", rcv_message);
+  printf("Receiver Task Reads : #%u\r\n", rcv_message);
   TerminateTask();
 }
 
@@ -32,7 +32,6 @@ TASK(sender)
 #elif IOC_QUEUED_COUNT > 0
   IocSend_my_ioc_name(snd_message);
 #endif
-  printf("! Sender Task Sends : #%d\r\n", snd_message);
+  printf("! Sender Task Sends : #%u\r\n", snd_message);
   TerminateTask();
 }
-
