08427206c96b256196ab9021d53a0b4bc4df40c3
Jean-Luc Béchennec <jean-luc.bechennec@ls2n.fr>
2023-12-02 11:32:58 +0100

    [GOIL] build scripts for OS X

---
+++ goil/makefile-macosx/build.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+import sys, os, json
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+#----------------------------------------------------------------- Get script absolute path
+scriptDir = os.path.dirname (os.path.abspath (sys.argv [0]))
+os.chdir (scriptDir)
+#----------------------------------------------------------------- Get goal as first argument
+goal = "all" # Default goal
+if len (sys.argv) > 1 :
+  goal = sys.argv [1]
+#----------------------------------------------------------------- Get max parallel jobs as second argument
+maxParallelJobs = 0 # 0 means use host processor count
+if len (sys.argv) > 2 :
+  maxParallelJobs = int (sys.argv [2])
+#----------------------------------------------------------------- Get json description dictionary
+jsonFilePath = os.path.normpath (scriptDir + "/../build/output/file-list.json")
+with open (jsonFilePath) as f:
+  dictionary = json.loads (f.read ())
+LIBPM_DIRECTORY_PATH = dictionary ["LIBPM_DIRECTORY_PATH"]
+#----------------------------------------------------------------- Import builder
+sys.path.append (os.path.abspath (LIBPM_DIRECTORY_PATH + "/python-makefiles"))
+from macosx_gcc_tools import buildForMacOS
+#----------------------------------------------------------------- Build
+buildForMacOS (dictionary, jsonFilePath, "goil", "build", goal, maxParallelJobs, maxParallelJobs == 1)
+
+#-----------------------------------------------------------------------------------------------------------------------
---
+++ goil/makefile-macosx/clean.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+import sys, os, subprocess, atexit
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+def cleanup():
+  if childProcess.poll () == None :
+    childProcess.kill ()
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+#--- Register a function for killing subprocess
+atexit.register (cleanup)
+#--- Get script absolute path
+scriptDir = os.path.dirname (os.path.abspath (sys.argv [0]))
+#---
+childProcess = subprocess.Popen (["/usr/bin/python3", "build.py", "clean"], cwd=scriptDir)
+#--- Wait for subprocess termination
+if childProcess.poll () == None :
+  childProcess.wait ()
+if childProcess.returncode != 0 :
+  sys.exit (childProcess.returncode)
+
+#-----------------------------------------------------------------------------------------------------------------------
