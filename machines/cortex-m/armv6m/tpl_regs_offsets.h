d12ff8847fa15c947e30fb6be1181aaeaa4b7d3d
Mikaël BRIDAY <mikael.briday@univ-nantes.fr>
2024-02-28 16:18:16 +0100

    [FPU] update cortexM0/M3 for tpl_load_context prototype

---
+++ machines/cortex-m/armv6m/tpl_regs_offsets.h
@@ -0,0 +1,17 @@
+#ifndef __TPL_REGS_OFFSETS_H__
+#define __TPL_REGS_OFFSETS_H__
+
+#define CTX_GPR4 0
+#define CTX_GPR5 4
+#define CTX_GPR6 8
+#define CTX_GPR7 12
+#define CTX_GPR8 16
+#define CTX_GPR9 20
+#define CTX_GPR10 24
+#define CTX_GPR11 28
+#define CTX_PSP 32
+
+#define INT_CONTEXT 0
+#define FLOAT_CONTEXT 4
+
+#endif
