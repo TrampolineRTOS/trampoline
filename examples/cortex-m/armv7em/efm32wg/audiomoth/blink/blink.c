35e321ad5da717e4f87b8f60ae556e55ee728556
bernabeu-a <antoine.bernabeu@zoho.com>
2025-06-26 11:28:31 +0200

    [audiomoth] fix blink example

--- examples/cortex-m/armv7em/efm32wg/audiomoth/blink/blink.c
+++ examples/cortex-m/armv7em/efm32wg/audiomoth/blink/blink.c
@@ -3,6 +3,8 @@
 #include "em_device.h"
 #include "em_chip.h"
 #include "em_cmu.h"
+#include "em_emu.h"
+#include "em_letimer.h"
 
 #define APP_Task_blink_START_SEC_CODE
 #include "tpl_memmap.h"
@@ -13,12 +15,20 @@ FUNC(int, OS_APPL_CODE) main(void){
 	CMU_ClockEnable(cmuClock_GPIO, true);
 	// Set GPIO Red Led to Output
 	GPIO->P[gpioPortC].MODEL |= 4 << (16);
+	GPIO->P[gpioPortC].MODEL |= 4 << (20);
+	/* Enable high frequency HFXO clock */
+	CMU_OscillatorEnable(cmuOsc_HFXO, true, true);
+	CMU_ClockDivSet(cmuClock_HF, cmuClkDiv_1);
+	CMU_ClockSelectSet(cmuClock_HF, cmuSelect_HFXO);
+	CMU_OscillatorEnable(cmuOsc_HFRCO, false, false);
+
+	/* Enable clock to Low energy module */
+	CMU_ClockEnable(cmuClock_CORELE, true);
 	StartOS(OSDEFAULTAPPMODE);
 	return 0;
 }
 
 TASK(blink){
-	
 	GPIO->P[gpioPortC].DOUT ^= 1<<4;
 	TerminateTask();
 }
